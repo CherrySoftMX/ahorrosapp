cff07ed7095c92cb7150cb274d360abb1efa78dd
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-06 17:38:55 -0600

    Removed username in path variable for SavingsSummaryController class

--- web/src/main/java/com/cherrysoft/ahorrosapp/controllers/SavingsSummaryController.java
+++ web/src/main/java/com/cherrysoft/ahorrosapp/controllers/SavingsSummaryController.java
@@ -6,6 +6,7 @@ import com.cherrysoft.ahorrosapp.dtos.summaries.IntervalSavingsSummaryDTO;
 import com.cherrysoft.ahorrosapp.dtos.summaries.PiggyBankSummaryDTO;
 import com.cherrysoft.ahorrosapp.hateoas.assemblers.IntervalSavingsSummaryAssembler;
 import com.cherrysoft.ahorrosapp.hateoas.assemblers.PiggyBankSummaryAssembler;
+import com.cherrysoft.ahorrosapp.security.SecurityUser;
 import com.cherrysoft.ahorrosapp.services.SavingsSummaryService;
 import com.cherrysoft.ahorrosapp.utils.ExtraMediaTypes;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -22,17 +24,18 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping(SavingsSummaryController.BASE_URL)
 public class SavingsSummaryController {
-  public static final String BASE_URL = "{ownerUsername}/{pbName}/summary";
+  public static final String BASE_URL = "/{pbName}/summary";
   private final SavingsSummaryService savingsSummaryService;
   private final PiggyBankSummaryAssembler pbSummaryAssembler;
   private final IntervalSavingsSummaryAssembler intervalSavingsSummaryAssembler;
 
   @GetMapping
   public IntervalSavingsSummaryDTO getIntervalSavingsSummaryAsJson(
-      @PathVariable String ownerUsername,
+      @AuthenticationPrincipal SecurityUser loggedUser,
       @PathVariable String pbName,
       @RequestParam Map<String, String> summaryOptions
   ) {
+    String ownerUsername = loggedUser.getUsername();
     var spec = new SavingsSummarySpec(ownerUsername, pbName, summaryOptions);
     var result = savingsSummaryService.getIntervalSavingsSummary(spec);
     return intervalSavingsSummaryAssembler.toModel(result);
@@ -40,10 +43,11 @@ public class SavingsSummaryController {
 
   @GetMapping(produces = ExtraMediaTypes.APPLICATION_EXCEL_VALUE)
   public ResponseEntity<ByteArrayResource> getIntervalSavingsSummaryAsXlsx(
-      @PathVariable String ownerUsername,
+      @AuthenticationPrincipal SecurityUser loggedUser,
       @PathVariable String pbName,
       @RequestParam Map<String, String> summaryOptions
   ) {
+    String ownerUsername = loggedUser.getUsername();
     String fileName = "test";
     var spec = new SavingsSummarySpec(ownerUsername, pbName, summaryOptions);
     byte[] result = savingsSummaryService.getIntervalSavingsSummaryAsXlsx(spec);
@@ -57,9 +61,10 @@ public class SavingsSummaryController {
 
   @GetMapping("/total")
   public PiggyBankSummaryDTO getPiggyBankSummaryAsJson(
-      @PathVariable String ownerUsername,
+      @AuthenticationPrincipal SecurityUser loggedUser,
       @PathVariable String pbName
   ) {
+    String ownerUsername = loggedUser.getUsername();
     var spec = new SavingsSummarySpec(ownerUsername, pbName);
     PiggyBankSummary result = savingsSummaryService.getPiggyBankSummary(spec);
     return pbSummaryAssembler.toModel(result);

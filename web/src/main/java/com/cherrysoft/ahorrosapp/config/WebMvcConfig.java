75281e9dff01d079749b44b654ff55906db9f937
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-09 18:19:03 -0600

    Added RequestLoggerInterceptor class

---
+++ web/src/main/java/com/cherrysoft/ahorrosapp/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.cherrysoft.ahorrosapp.config;
+
+import com.cherrysoft.ahorrosapp.logging.RequestLoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new RequestLoggerInterceptor());
+  }
+
+}

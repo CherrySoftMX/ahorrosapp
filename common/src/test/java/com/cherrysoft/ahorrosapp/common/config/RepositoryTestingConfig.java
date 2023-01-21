2f5e9c36575566f5a464391ca45b51d86fd2f94e
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-21 17:06:08 -0600

    Fixed RepositoryTestingConfig scan packages

--- common/src/test/java/com/cherrysoft/ahorrosapp/common/config/RepositoryTestingConfig.java
+++ common/src/test/java/com/cherrysoft/ahorrosapp/common/config/RepositoryTestingConfig.java
@@ -3,7 +3,7 @@ package com.cherrysoft.ahorrosapp.common.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan("com.cherrysoft.ahorrosapp")
-@EnableJpaRepositories("com.cherrysoft.ahorrosapp")
+@EntityScan("com.cherrysoft.ahorrosapp.common")
+@EnableJpaRepositories("com.cherrysoft.ahorrosapp.common")
 public class RepositoryTestingConfig {
 }

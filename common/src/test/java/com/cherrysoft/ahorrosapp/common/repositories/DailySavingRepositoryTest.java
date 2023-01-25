34ca71d701f908f44968194a983a5e0a771209ee
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-24 18:23:45 -0600

    Added integration tests for daily savings repository

---
+++ common/src/test/java/com/cherrysoft/ahorrosapp/common/repositories/DailySavingRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.cherrysoft.ahorrosapp.common.repositories;
+
+import com.cherrysoft.ahorrosapp.common.config.RepositoryTestingConfig;
+import com.cherrysoft.ahorrosapp.common.core.models.DailySaving;
+import com.cherrysoft.ahorrosapp.common.core.models.PiggyBank;
+import com.cherrysoft.ahorrosapp.common.core.models.SavingsDateRange;
+import com.cherrysoft.ahorrosapp.common.utils.TestUtils;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@ContextConfiguration(classes = {DailySavingRepository.class, PiggyBankRepository.class})
+@Import(RepositoryTestingConfig.class)
+@ActiveProfiles("test")
+class DailySavingRepositoryTest {
+  @Autowired private DailySavingRepository dailySavingRepository;
+  @Autowired private PiggyBankRepository pbRepository;
+  private PiggyBank pb;
+
+  @BeforeEach
+  void setup() {
+    String january2023 = "01-2023";
+    PiggyBank newPb = PiggyBank.builder()
+        .name("test-piggy")
+        .savingsDateRange(SavingsDateRange.forMonth(january2023))
+        .build();
+    List<DailySaving> savings = TestUtils.Savings.generateSavingsForMonth(january2023);
+    newPb.addDailySavings(savings);
+    pb = pbRepository.saveAndFlush(newPb);
+  }
+
+  @AfterEach
+  void deleteAll() {
+    dailySavingRepository.deleteAll();
+    pbRepository.deleteAll();
+  }
+
+  @Nested
+  @DisplayName("Find daily saving by date")
+  class FindDailySavingByDate {
+
+    @Test
+    void shouldReturnDailySaving() {
+      Optional<DailySaving> firstMonthSaving = dailySavingRepository.findByPiggyBankAndDate(pb, LocalDate.of(2023, Month.JANUARY, 1));
+
+      assertTrue(firstMonthSaving.isPresent());
+    }
+
+    @Test
+    void shouldReturnEmptyDailySaving_whenInvalidDate() {
+      LocalDate invalidDate = LocalDate.of(2012, Month.DECEMBER, 19);
+      Optional<DailySaving> maybeSaving = dailySavingRepository.findByPiggyBankAndDate(pb, invalidDate);
+
+      assertTrue(maybeSaving.isEmpty());
+    }
+
+  }
+
+  @Nested
+  @DisplayName("Find daily savings between dates")
+  class FindDailySavingsBetweenDates {
+
+    @Test
+    void shouldReturnDailySavingsBetweenDates() {
+      List<DailySaving> dailySavings = dailySavingRepository.findByPiggyBankAndDateBetween(pb, LocalDate.of(2023, Month.JANUARY, 1), LocalDate.of(2023, Month.JANUARY, 7));
+
+      assertThat(dailySavings, hasSize(7));
+    }
+
+  }
+
+
+}
--- common/src/test/java/com/cherrysoft/ahorrosapp/common/utils/TestUtils.java
+++ common/src/test/java/com/cherrysoft/ahorrosapp/common/utils/TestUtils.java
@@ -2,11 +2,10 @@ package com.cherrysoft.ahorrosapp.common.utils;
 
 import com.cherrysoft.ahorrosapp.common.core.models.DailySaving;
 import com.cherrysoft.ahorrosapp.common.core.models.PiggyBank;
+import com.cherrysoft.ahorrosapp.common.core.models.SavingsDateRange;
 import com.cherrysoft.ahorrosapp.common.core.utils.MonthParser;
-import com.cherrysoft.ahorrosapp.common.core.models.User;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -14,89 +13,29 @@ import static java.util.stream.Collectors.toList;
 
 public class TestUtils {
 
-  public static class Users {
-
-    public static User newUser() {
-      return User.builder()
-          .username("hiking")
-          .password("password")
-          .pbs(new ArrayList<>())
-          .build();
-    }
-
-    public static List<User> newUsers() {
-      return List.of(
-          User.builder()
-              .username("javier")
-              .password("12345")
-              .pbs(new ArrayList<>())
-              .build(),
-          User.builder()
-              .username("diego")
-              .password("password")
-              .pbs(new ArrayList<>())
-              .build(),
-          User.builder()
-              .username("nicolas")
-              .password("password123")
-              .pbs(new ArrayList<>())
-              .build()
-      );
-    }
-
-    public static User savedUser() {
-      User newUser = newUser();
-      return User.builder()
-          .id(1L)
-          .username(newUser.getUsername())
-          .password(newUser.getPassword())
-          .pbs(new ArrayList<>())
-          .build();
-    }
-  }
-
   public static class PiggyBanks {
-
-    public static PiggyBank newPiggyBank() {
-      return PiggyBank.builder()
-          .name("piggy")
-          .startSavings(DateUtils.today())
-          .endSavings(DateUtils.today().plusDays(7))
-          .build();
-    }
 
     public static PiggyBank newPiggyBankNoEndDate() {
       return PiggyBank.builder()
           .name("piggy")
-          .startSavings(DateUtils.today())
+          .savingsDateRange(new SavingsDateRange())
           .build();
-    }
-
-    public static PiggyBank newPiggyBankNoStartDate() {
-      return PiggyBank.builder()
-          .name("piggy")
-          .endSavings(DateUtils.today().plusDays(5))
-          .build();
-    }
-
-    public static PiggyBank newPiggyBankWithOwner() {
-      User owner = Users.newUser();
-      PiggyBank pb = newPiggyBank();
-      pb.setOwner(owner);
-      owner.addPiggyBank(pb);
-      return pb;
     }
 
   }
 
   public static class Savings {
 
+    public static List<DailySaving> generateSavingsForMonth(String month) {
+      return generateSavingsForMonth(month, 0);
+    }
+
     public static List<DailySaving> generateSavingsForMonth(String month, double amountPerDay) {
       MonthParser monthParser = new MonthParser(month);
       int totalMonthDays = monthParser.endOfMonth().getDayOfMonth();
       return IntStream.range(0, totalMonthDays)
-          .mapToObj(value -> new DailySaving(
-              monthParser.startOfMonth().plusDays(value),
+          .mapToObj(i -> new DailySaving(
+              monthParser.startOfMonth().plusDays(i),
               BigDecimal.valueOf(amountPerDay)
           ))
           .collect(toList());

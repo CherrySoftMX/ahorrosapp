d908120bd399291888d8a33dbfc3d2c6ef367042
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-27 19:09:53 -0600

    Added integration tests for daily savings controller & fixed date formatting bugs

--- common/src/main/java/com/cherrysoft/ahorrosapp/common/services/exceptions/saving/SavingOutOfDateRangeException.java
+++ common/src/main/java/com/cherrysoft/ahorrosapp/common/services/exceptions/saving/SavingOutOfDateRangeException.java
@@ -4,10 +4,12 @@ import com.cherrysoft.ahorrosapp.common.core.models.SavingsDateRange;
 
 import java.time.LocalDate;
 
+import static com.cherrysoft.ahorrosapp.common.utils.DateUtils.formatDate;
+
 public class SavingOutOfDateRangeException extends RuntimeException {
 
   public SavingOutOfDateRangeException(LocalDate savingDate, SavingsDateRange savingsDateRange) {
-    super(String.format("The saving with date: %s is out of date range: %s", savingDate, savingsDateRange));
+    super(String.format("The saving with date: %s is out of date range: %s", formatDate(savingDate), savingsDateRange));
   }
 
 }

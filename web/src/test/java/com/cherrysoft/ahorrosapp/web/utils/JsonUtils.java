44d51279f09ce16bde7757d0784ecfd15c16c8c2
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-30 23:08:55 -0600

    Removed unused method `readValueFromJsonString()` method in JsonUtils class

--- web/src/test/java/com/cherrysoft/ahorrosapp/web/utils/JsonUtils.java
+++ web/src/test/java/com/cherrysoft/ahorrosapp/web/utils/JsonUtils.java
@@ -3,7 +3,6 @@ package com.cherrysoft.ahorrosapp.web.utils;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtils {
@@ -14,15 +13,6 @@ public class JsonUtils {
       mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
       mapper.registerModule(new JavaTimeModule());
       return mapper.writeValueAsString(object);
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public static String readValueFromJsonString(String json, String key) {
-    try {
-      ObjectNode node = new ObjectMapper().readValue(json, ObjectNode.class);
-      return node.get(key).asText();
     } catch (JsonProcessingException e) {
       throw new RuntimeException(e);
     }
